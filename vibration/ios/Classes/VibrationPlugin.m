b46c31e25a9a1b49108ca32b1b114ac1e5b2bbd6
lizubing1992 <lizubing1992@163.com>
2021-04-01 15:00:53 +0800

    修改成OC

--- vibration/ios/Classes/VibrationPlugin.m
+++ vibration/ios/Classes/VibrationPlugin.m
@@ -1,12 +1,37 @@
 #import "VibrationPlugin.h"
-#if __has_include(<vibration/vibration-Swift.h>)
-#import <vibration/vibration-Swift.h>
-#else
-#import "vibration-Swift.h"
-#endif
+#import <AudioToolbox/AudioToolbox.h>
 
 @implementation VibrationPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [VibrationPluginSwift registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+        methodChannelWithName:@"vibration"
+              binaryMessenger:[registrar messenger]];
+    VibrationPlugin* instance = [[VibrationPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else if([@"hasVibrator" isEqualToString:call.method]){
+      result(@TRUE);
+  }else if([@"hasAmplitudeControl" isEqualToString:call.method]){
+      result(@TRUE);
+  }else if([@"hasCustomVibrationsSupport" isEqualToString:call.method]){
+      result(@TRUE);
+  }else if([@"vibrate" isEqualToString:call.method]){
+      if (@available(iOS 10.0, *)) {
+          UIImpactFeedbackGenerator *generator = [[UIImpactFeedbackGenerator alloc] initWithStyle:UIImpactFeedbackStyleHeavy];
+          [generator impactOccurred];
+      } else {
+          // Fallback on earlier versions
+          AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+      }
+        
+  }else if([@"cancel" isEqualToString:call.method]){
+      result(@"");
+  }else{
+    result(FlutterMethodNotImplemented);
+  }
 }
 @end
--- vibration/ios/vibration.podspec
+++ vibration/ios/vibration.podspec
@@ -18,5 +18,4 @@ Handle vibration on iOS and Android
   s.platform = :ios, '8.0'
 
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'VALID_ARCHS[sdk=iphonesimulator*]' => 'x86_64' }
-  s.swift_version = '5.3'
 end
